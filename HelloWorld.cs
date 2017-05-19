4f3821eeea9e2fb4b63b399a7277d653d1b3af07
Kapaaaau <juanpaulo10@yahoo.com>
2017-05-19 19:53:39 +0800

    enum anonymousmethod lambdaexpr fileIO

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -3,226 +3,172 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
+
+delegate double GetSum(double num1, double num2);
 
 namespace CSharpHelloWorld
 {
-    //CLASSES AND OBJECTS
-    class Animal
+    public enum Temperature
     {
-        // properties and their automatic getters and setters
-        public double height { get; set; }
-        public double weight { get; set; }
-        public string sound { get; set; }
+        Freeze,
+        Low,
+        Warm,
+        Boil
+    }
 
-        //creating own getters and setters
-        public string name;
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
+    struct Customers
+    {
+        private string name;
+        private double balance;
+        private int id;
 
-        //constructor
-        public Animal()
+        public void createCust(string name, double bal, int id)
         {
-            this.height = 0;
-            this.weight = 0;
-            this.name = "No Name";
-            this.sound = "No Sound";
-            numOfAnimals++;
-        }
-
-        public Animal(double height, double weight, string name, string sound)
-        {
-            this.height = height;
-            this.weight = weight;
             this.name = name;
-            this.sound = sound;
-            numOfAnimals++;
+            this.balance = bal;
+            this.id = id;
         }
 
-        static int numOfAnimals = 0;
-
-        static int getNumOfAnimals()
+        public void showCustInfo()
         {
-            return numOfAnimals;
+            Console.WriteLine("Name " + name);
+            Console.WriteLine("Balance " + balance);
+            Console.WriteLine("Id " + id);
         }
+    }
 
-        public string introduce()
-        {
-            return String.Format("{0} is {1} inches tall, weighs {2} lbs and likes to say {3}", name, height, weight, sound);
-        }
-
-        public int getSum(int num1 = 1, int num2 = 1)
-        {
-            return num1 + num2;
-        }
-
-        public double getSum(double num1 = 1, double num2 = 1)
-        {
-            return num1 + num2;
-        }
-
+    class Program
+    {
+        
         static void Main(string[] args)
         {
-            //for classes examples
-            /*Animal Sando = new Animal (15, 10, "Sando", "woof");
-            Animal Tim = new Animal(5, 3, "Tim", "arf");
+            /*Temperature micTemp = Temperature.Warm;
 
-            Console.WriteLine("{0} says {1}", Sando.name, Sando.sound);
-            Console.WriteLine("{0} says {1}", Tim.name, Tim.sound);
-
-            Console.WriteLine("Number of animals created:" + Animal.getNumOfAnimals());
-
-            Console.WriteLine(Sando.introduce());
-            Console.WriteLine(Tim.introduce());
-
-            Console.WriteLine(Sando.getSum(num2: 1.3, num1: 2.6));
-
-            Animal pichi = new Animal
+            switch (micTemp)
             {
-                name = "pichi",
-                height = 16,
-                weight = 18,
-                sound = "ror"
+                case Temperature.Freeze:
+                    Console.WriteLine("Temp on Freezing");
+                    break;
+                case Temperature.Low:
+                    Console.WriteLine("Temp on Low");
+                    break;
+                case Temperature.Warm:
+                    Console.WriteLine("Temp on Warm");
+                    break;
+                case Temperature.Boil:
+                    Console.WriteLine("Temp on Boiling");
+                    break;
+            }*/
+
+            /*Customers bob = new Customers();
+            bob.createCust("Bob", 15.50 ,111345);
+            bob.showCustInfo();*/
+
+            //anonymous method/function. SEE delegate on top
+            /*GetSum sum = delegate(double num1, double num2)
+            {
+                return num1 + num2;
             };
 
-            Dog spike = new Dog();
-            Console.WriteLine(spike.introduce());
+            Console.WriteLine("5 + 10 =" +sum(5, 10));*/
 
-            spike = new Dog(20, 15, "Spike", "arfu arf", "burger");
-            Console.WriteLine(spike.introduce());*/
+            //lambda expression, 3 params -- 2 for args, 1 for result
+            /*Func<int, int, int> getSum = (x, y) => x + y;
 
+            Console.WriteLine("5 + 3 = " + getSum.Invoke(5, 3));
+
+            List<int> numList = new List<int> { 5, 10 , 20, 15, 25};
+            //Where(Lamba Expression as argument)
+            List<int> oddNums = numList.Where(n => n % 2 == 1).ToList();*/
+
+            //FILE I/O
+            /*string[] custs = new string[] { "Tom", "Paul", "Greg" };
+            FileStream SourceStream = new FileStream("custs.txt", FileMode.Create);
+            using (StreamWriter sw = new StreamWriter(SourceStream))
+            {
+                foreach (string cust in custs)
+                {
+                    sw.WriteLine(cust);
+                }
+            }
+            SourceStream.Dispose();
             
+            string custName = "";
 
-            //for polymorphism/inheritance - abstract/interfaces
-            /*Shape rect = new Rectangle(5, 5);
-            Shape tri = new Triangle(5, 5);
+            FileStream SourceStream2 = new FileStream("custs.txt", FileMode.Open);
+            // Read the file and display it line by line.
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(SourceStream2))
+            {
+                while((custName = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(custName);
+                }
+            }
 
-            Console.WriteLine("Rect Area " + rect.area());
-            Console.WriteLine("Tri Area " + tri.area());
-
-            Rectangle combineRect = new Rectangle(5, 6) + new Rectangle(5, 8);
-            Console.WriteLine("combineRect Area " + combineRect.area());*/
-
-
-
-            //for Generics
-            KeyValue<string, string> superman = new KeyValue<string, string>("", "");
-            superman.key = "Superman";
-            superman.value = "Clark Kent";
-
-            KeyValue<int, string> samsungTV = new KeyValue<int, string>(0, "");
-            samsungTV.key = 11134771;
-            samsungTV.value = "a 50 inch Samsung TV";
-
-            superman.showData();
-            samsungTV.showData();
+            SourceStream2.Dispose();*/
+            
+            /*Employee e1 = new Employee("Bob", "120 k-j street", 12345);
+            e1.introduce();*/
         }
     }
 
-    class Dog : Animal
+    /*class Company
     {
-        //properties
-        public string favFood { get; set; }
+        private string name { get; set; }
+        private string addr { get; set; }
+        static public int numEmployees;
 
-        //calls super class 
-        public Dog() : base()
+        public Company()
         {
-            this.favFood = "No Fave Food";
+            this.name = "No Name";
+            this.addr = "No Address";
         }
 
-        public Dog (double height, double weight, string name, string sound, string favFood) : base(height, weight, name, sound)
+        public Company(string name, string addr)
         {
-            this.favFood = favFood;
+            this.name = name;
+            this.addr = addr;
         }
 
-        //overriding in c# has keyword "new"
-        new public string introduce()
+        public static int getNumEmployees()
         {
-            return String.Format("{0} is {1} inches tall, weighs {2} lbs and likes to say {3} and eats {4}", name, height, weight, sound, favFood);
+            return numEmployees;
         }
     }
 
-    //abstract methods are methods that should be defined by a class
-    //ONE abstract class can only be inherited by a class
-    abstract class Shape
+    class Employee : Company
     {
-        public abstract double area();
+        private string name { get; set; }
+        private string addr { get; set; }
+        private int id { get; set; }
 
-        public void sayHi()
+        public Employee(string name, string addr, int id)
         {
-            Console.WriteLine("Hello");
+            this.name = name;
+            this.addr = addr;
+            this.id = id;
+            numEmployees++;
         }
-    }
-
-    //every method of an interface is an abstract method
-    public interface ShapeItem
-    {
-        double area();
-    }
-
-    class Rectangle : Shape
-    {
-        private double length;
-        private double width;
-
-        //constructor
-        public Rectangle(double num1, double num2)
+        
+        public string getName()
         {
-            length = num1;
-            width = num2;
+            return name;
         }
 
-        public override double area()
+        public string getAddr()
         {
-            return length * width;
+            return addr;
         }
 
-        //operator overloading - example: adding rectangle together
-        public static Rectangle operator+ (Rectangle rect1, Rectangle rect2)
+        public int getId()
         {
-            double rectLength = rect1.length + rect2.length;
-            double rectWidth = rect1.width + rect2.width;
-
-            return new Rectangle(rectLength, rectWidth);
-        }
-    }
-
-    class Triangle : Shape
-    {
-        private double triangleBase;
-        private double height;
-
-        //constructor
-        public Triangle(double num1, double num2)
-        {
-            triangleBase = num1;
-            height = num2;
+            return id;
         }
 
-        public override double area()
+        public void introduce()
         {
-            return .5 * (triangleBase * height);
+            Console.WriteLine("{0}-ID{1} is added to the company, he lives in {2}.\nTotal Num Employees: {3}", this.getName(), this.getId(), this.getAddr(), Company.getNumEmployees());
         }
-    }
-
-    //Generic
-    class KeyValue<TKey, TValue>
-    {
-        public TKey key { get; set; }
-        public TValue value { get; set; }
-
-        public KeyValue(TKey k, TValue v)
-        {
-            key = k;
-            value = v;
-        }
-
-        public void showData()
-        {
-            Console.WriteLine("{0} is {1} ", this.key, this.value);
-        }
-    }
-
+    }*/
 }
